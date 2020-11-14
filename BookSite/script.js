32796c2e1273a8b543b4497d5fb5f189da1e9b6c
Odessaboy <alexwebcourses@gmail.com>
2020-11-14 15:09:37 +0200

    Js  logo and css changes
    +++++

--- BookSite/script.js
+++ BookSite/script.js
@@ -5,47 +5,37 @@ burger.addEventListener('click', function () {
     menu.classList.toggle('active');
 });
 
-  window .onscroll = function(){
-    let top = window.ScrollY > 0;
-    let small = document.getElementsByClassName('logo')[0];
-    small .classList.toggle('change', top);
-    
-  };
 
-
-
-// Homepage order items
-
- let orderitem = document.getElementById('order_book').getElementsByClassName('item');
- for (i=0;i<orderitem.length;i++){
- let itemanimate = orderitem[i];
-  itemanimate .addEventListener('mouseover', function () {
-    itemanimate .classList.add('animate');
+window.addEventListener('scroll', function(){
+   let small = document.getElementsByClassName('logo')[0];
+   let top = window.scrollY;
+   if (top>150){
+     small.classList .add('change');
+   }
+   else{
+    small.classList.remove('change'); 
+   }
+   
   });
-  itemanimate .addEventListener('mouseout', function () {
-    
-  itemanimate .classList.remove('animate');
 
+
+
+// Animate items
+let orderitem = document.getElementsByClassName('item');
+// let number = document.getElementById('order_book').getElementsByTagName('span');
+for (i=0;i<orderitem.length;i++){
+ let itemanimate = orderitem[i];
+itemanimate .addEventListener('mouseover', function () {
+itemanimate .classList.add('animate');});
+  
+itemanimate .addEventListener('mouseout', function () {
+itemanimate .classList.remove('animate');
  });
 
- }
-//  orderitem .addEventListener('mouseover', function () {
- 
-// orderitem .classList.add('animate');
- 
-//  let number = document.getElementById('order_book').getElementsByTagName('span')[0];
-//          number. classList.add('active'); 
-//     });
+}
 
- 
-//     orderitem .addEventListener('mouseout', function () {
-    
-//    orderitem .classList.remove('animate');
-    
-//    let number = document.getElementById('order_book').getElementsByTagName('span')[0];
-//             number. classList.remove('active'); 
-//        });
-   
+
+
 
 
 
