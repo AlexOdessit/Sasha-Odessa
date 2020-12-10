9d14e586098b32e2fc196eca8a05e722de268360
Odessaboy <alexwebcourses@gmail.com>
2020-12-10 17:24:09 +0200

    localStorage +++
    +++

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,19 +1,36 @@
 $(document).ready(function () {
   let counter = $('.get_cart p');
   let prdCount = localStorage.getItem('cartCount');
-  if (prdCount) {
+  if (prdCount != null) {
     counter.text(prdCount);
   }
+
   $('button').on('click', function () {
     let my_storage = localStorage.getItem('products');
     let name = $(this).parent('.product').find('h1').text();
     let img = $(this).parent('.product').find('img').attr('src');
     let old = $(this).parent('.product').find('.price p span').text();
     let sale = $(this).parent('.product').find('.price h3 span').text();
-    let color = $(this).parent('.product').find('input');
-    let colorz = [];
-    for (i = 0; i < color.length; i++) {
-      colorz.push(color[i].value);
+    let model = $(this).parent('.product').find('.model');
+    let arrm = '';
+    for (i = 0; i < model.length; i++) {
+      arrm = $(model[i]).text();
+    }
+    // let year = [];
+    // for (i = 0; i < model.length; i++) {
+    //   year.push($(model[i]).text());
+    // }
+    let color = $('input[name="color"]:checked').val();
+    if ($('input[name="color"]').is(':checked')) {
+      $('input[name="color"]:checked').val();
+    } else {
+      alert('Choose color');
+    }
+    let size = $(this).parent('.product').find('input[name = size]').val();
+    if (size === null) {
+      alert('choose size');
+    } else {
+      alert(size);
     }
 
     //  localStorage
@@ -23,7 +40,9 @@ $(document).ready(function () {
       oldprice: old,
       saleprice: parseInt(sale),
       count: 1,
-      colors: colorz,
+      colorz: color,
+      sizes: size,
+      models: arrm,
     };
     if (my_storage === null) {
       let my_products = {
@@ -63,77 +82,69 @@ $(document).ready(function () {
     }
     totalCost();
     // Счетчик товаров в корзине
-    let counter = $('.get_cart p');
-    let prdCount = localStorage.getItem('cartCount');
-
-    let cart = $('.cart_box');
-    let closebtn = $('.close_btn');
-    $('.get_cart').on('click', function () {
-        my_str1 = JSON.parse(localStorage.getItem('products'));
-        if (localStorage.getItem('products') === null || my_str1.products.length == 0) {
-            $('.pop_up_overlay').addClass('active');
-            setTimeout(function(){
-                $('.pop_up_overlay').removeClass('active');
-            },2000);
-        } else {
-            cart.addClass('active');
-            let cartItems = localStorage.getItem('products');
-            let cartContent = $('.product_content');
-            let cartCost = localStorage.getItem('totalCost');
-            cartItems = JSON.parse(cartItems);
-            cartItems = cartItems.products;
-            cartItems.map(function (p) {
-                cartContent.append(
-                    '<div class="product_content"><h3>' +
-                    p.title +
-                    '</h3> <img src = "' +
-                    p.image +
-                    '"><span>Price:' +
-                    p.saleprice +
-                    '$</span><span>' +
-                    p.colors +
-                    '</span><span>quantity:' +
-                    p.count +
-                    '</span></div>'
-                );
-            });
-            $('.cart_box').append('<h2 class ="total"> Total  ' + cartCost + '$</h2>');
-
-  let cart = $('.cart_box');
-  let closebtn = $('.close_btn');
-  $('.get_cart').on('click', function () {
-    my_str1 = JSON.parse(localStorage.getItem('products'));
-    if (localStorage.getItem('products') === null || my_str1.products.length == 0) {
-      alert('No products');
-    } else {
-      cart.addClass('active');
-      let cartItems = localStorage.getItem('products');
-      let cartContent = $('.product_content');
-      let cartCost = localStorage.getItem('totalCost');
-      cartItems = JSON.parse(cartItems);
-      cartItems = cartItems.products;
-      cartItems.map(function (p) {
-        cartContent.append(
-          '<div class="product_content"><h3>' +
-            p.title +
-            '</h3> <img src = "' +
-            p.image +
-            '"><span>Price:' +
-            p.saleprice +
-            '$</span><span>' +
-            p.colors +
-            '</span><span>quantity:' +
-            p.count +
-            '</span></div>'
-        );
-      });
-      $('.cart_box').append('<h2 class ="total"> Total  ' + cartCost + '$</h2>');
+    function cartCounter() {
+      let prdCount = localStorage.getItem('cartCount');
+      if (prdCount != null) {
+        prdCount = parseInt(prdCount);
+        localStorage.setItem('cartCount', prdCount + 1);
+        counter.text(prdCount + 1);
+      } else {
+        localStorage.setItem('cartCount', 1);
+        counter.text((prdCount = 1));
+      }
     }
+    cartCounter();
   });
 
+  let cart = $('.cart_box');
+  $('.get_cart').on('click', function () {
+    my_str1 = JSON.parse(localStorage.getItem('products'));
+    if (localStorage.getItem('products') === null || my_str1.products.length === 0) {
+      $('.pop_up_overlay').addClass('active');
+      setTimeout(function () {
+        $('.pop_up_overlay').removeClass('active');
+      }, 3000);
+    } else {
+      cart.addClass('active');
+    }
+    let cartItems = JSON.parse(localStorage.getItem('products'));
+    let cartContent = $('.product_content');
+    let cartCost = localStorage.getItem('totalCost');
+    cartItems = cartItems.products;
+    cartItems.map(function (p) {
+      cartContent.append(
+        '<div class="product_content"><h3>' +
+          p.title +
+          '</h3> <img src = "' +
+          p.image +
+          '"><span>Price:' +
+          p.saleprice +
+          '$</span><span>color:' +
+          p.colorz +
+          '</span><span>size:' +
+          p.sizes +
+          '</span><p>Models:' +
+          p.models +
+          '</p><span>quantity:' +
+          p.count +
+          '</span><button>remove item</button></div>'
+      );
+    });
+    $('.cart_box').append('<h3 class ="total"> Total:  ' + cartCost + '$</h3> <h3 class="count">Total products:' + prdCount + '</h3>');
+  });
+
+  $('body').on('click', '.cart_box button', function () {
+    $(this).parent('.product_content').remove();
+    let pIndex = $(this).parent('.product_content').index();
+    let items = JSON.parse(localStorage.getItem('products'));
+    items = items.products;
+    items.splice(pIndex, 1);
+  });
+
+  let closebtn = $('.close_btn');
   closebtn.on('click', function () {
     cart.removeClass('active');
     $('.product_content *').remove();
-    $('.total').remove();
+    $('.cart_box h3').remove();
   });
 });
